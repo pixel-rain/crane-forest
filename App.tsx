456000b8f51c80c7020e62eb019505827c20ef82
Evgenii Dergachev <edergachev@bk.ru>
2019-10-06 05:20:12 +1000

    began developing the app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+
+  const soundAssets = {
+    violin_A3_forte: require('./assets/sounds/violin_A3_forte.mp3'),
+    violin_B3_forte: require('./assets/sounds/violin_B3_forte.mp3'),
+    violin_C4_forte: require('./assets/sounds/violin_C4_forte.mp3'),
+  }
+
+  async function playSound(key: string) {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(soundAssets[key]);
+      await soundObject.playAsync();
+    } 
+    catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableHighlight style={styles.key} onPress={() => playSound('violin_A3_forte')}>
+        <Text></Text>
+      </TouchableHighlight>
+      <TouchableHighlight style={styles.key} onPress={() => playSound('violin_B3_forte')}>
+        <Text></Text>
+      </TouchableHighlight>
+      <TouchableHighlight style={styles.key} onPress={() => playSound('violin_C4_forte')}>
+        <Text></Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -16,4 +44,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  key: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'grey',
+    margin: 2
+  }
 });
