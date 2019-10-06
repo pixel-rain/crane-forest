4889140d51746687a9fbae85b9d0192eea3d7bc1
Evgenii Dergachev <edergachev@bk.ru>
2019-10-06 21:11:31 +1000

    more sounds, crude prototype of melody planning

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import { Audio } from 'expo-av';
 
 export default function App() {
 
+  const [notes, setNote] = useState({
+    violin_G3_05_forte: false, 
+    violin_A3_05_forte: false, 
+    violin_B3_05_forte: false, 
+    violin_C4_05_forte: false, 
+    violin_D4_05_forte: false, 
+    violin_E4_05_forte: false, 
+    violin_F4_05_forte: false, 
+    violin_G4_05_forte: false
+  });
+
   const soundAssets = {
-    violin_A3_forte: require('./assets/sounds/violin_A3_forte.mp3'),
-    violin_B3_forte: require('./assets/sounds/violin_B3_forte.mp3'),
-    violin_C4_forte: require('./assets/sounds/violin_C4_forte.mp3'),
+    violin_G3_05_forte: require('./assets/sounds/violin_G3_05_forte.mp3'),
+    violin_A3_05_forte: require('./assets/sounds/violin_A3_05_forte.mp3'),
+    violin_B3_05_forte: require('./assets/sounds/violin_B3_05_forte.mp3'),
+    violin_C4_05_forte: require('./assets/sounds/violin_C4_05_forte.mp3'),
+    violin_D4_05_forte: require('./assets/sounds/violin_D4_05_forte.mp3'),
+    violin_E4_05_forte: require('./assets/sounds/violin_E4_05_forte.mp3'),
+    violin_F4_05_forte: require('./assets/sounds/violin_F4_05_forte.mp3'),
+    violin_G4_05_forte: require('./assets/sounds/violin_G4_05_forte.mp3'),
   }
 
   async function playSound(key: string) {
@@ -21,17 +37,67 @@ export default function App() {
     }
   }
 
+  const buttons = [
+    'violin_G3_05_forte', 
+    'violin_A3_05_forte', 
+    'violin_B3_05_forte', 
+    'violin_C4_05_forte', 
+    'violin_D4_05_forte', 
+    'violin_E4_05_forte', 
+    'violin_F4_05_forte', 
+    'violin_G4_05_forte'
+  ].map(i => 
+    <TouchableHighlight 
+      style={{...styles.key, backgroundColor: notes[i] ? 'black' : 'white'}}
+      onPress={() => 
+        notes[i] ? setNote(prevState => {return {...prevState, [i]: false}}) : setNote(prevState => {return {...prevState, [i]: true}})
+      }
+      underlayColor={notes[i] ? "white" : "black"}
+      key={i}
+    >
+      <Text></Text>
+    </TouchableHighlight>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <TouchableHighlight style={styles.key} onPress={() => playSound('violin_A3_forte')}>
-        <Text></Text>
-      </TouchableHighlight>
-      <TouchableHighlight style={styles.key} onPress={() => playSound('violin_B3_forte')}>
-        <Text></Text>
-      </TouchableHighlight>
-      <TouchableHighlight style={styles.key} onPress={() => playSound('violin_C4_forte')}>
-        <Text></Text>
+      <View style={styles.keyBox}>
+        {buttons}
+      </View>
+      <TouchableHighlight 
+        style={styles.playButton} 
+        onPress={() => {
+          function runIteration(numTimes: number, delay: number) {
+            var cnt = 0;
+            function next() {
+              if (notes[keys[cnt]]) {
+                playSound(keys[cnt]);
+              }
+                cnt++;
+                if (cnt < numTimes) {
+                    setTimeout(next, delay);
+                }
+            }
+            next();
+        }
+        const keys = Object.keys(notes);
+        runIteration(keys.length, 500);
+
+
+
+
+
+          /*Object.keys(keys).forEach(async key => {
+
+
+            if (keys[key]) {
+
+              await playSound(key);
+            }
+          });*/
+        }}
+      >
+        <Text>Play!</Text>
       </TouchableHighlight>
     </View>
   );
@@ -47,7 +113,17 @@ const styles = StyleSheet.create({
   key: {
     width: 40,
     height: 40,
-    backgroundColor: 'grey',
-    margin: 2
+    margin: 2,
+    borderWidth: 1
+  },
+  keyBox: {
+    flexDirection: 'row'
+  },
+  playButton: {
+    borderWidth: 1,
+    padding: 4,
+    paddingLeft: 8,
+    paddingRight: 8,
+    marginTop: 5
   }
 });
