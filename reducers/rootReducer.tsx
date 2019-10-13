a0a016f26f36dc3698ddc6efff22e4b30840687b
Evgenii Dergachev <edergachev@bk.ru>
2019-10-14 05:04:29 +1000

    began adding melody playing, changed reducer

--- reducers/rootReducer.tsx
+++ reducers/rootReducer.tsx
@@ -1,31 +1,38 @@
 const UPDATE_MELODY = 'UPDATE_MELODY';
 
 const initialState = {
-    notes: {
-        violin_G3_05_forte: false, 
-        violin_A3_05_forte: false, 
-        violin_B3_05_forte: false, 
-        violin_C4_05_forte: false, 
-        violin_D4_05_forte: false,
-        violin_E4_05_forte: false, 
-        violin_F4_05_forte: false, 
-        violin_G4_05_forte: false,
-    },
+    notes: [],
+    x: 8,
+    y: 20
 };
 
+for (let i = 0; i < initialState.y; i++) {
+    const temp = [];
+    for (let j = 0; j < initialState.x; j++) {
+        temp.push(false);
+    }
+    initialState.notes.push(temp);
+}
+
 export interface State {
-    notes: object;
+    notes: boolean[][];
+    x: number;
+    y: number;
 }
 
 interface Action {
     type: string;
-    payload: object;
+    payload: {yPos: number, xPos: number, toPlay: boolean}; //y position is a place in the melody, x position is a note
 }
 
 export default function rootReducer(state: State = initialState, action: Action) {
     switch (action.type) {
-        case UPDATE_MELODY:
-            return {...state, notes: {...state.notes, ...action.payload}};
+        case 'UPDATE_MELODY':
+            const {yPos, xPos, toPlay} = action.payload;
+            const temp = [...state.notes];
+            temp[yPos] = [...temp[yPos]];
+            temp[yPos][xPos] = toPlay;
+            return {...state, notes: [...temp]};
         default:
             return state;
     }
